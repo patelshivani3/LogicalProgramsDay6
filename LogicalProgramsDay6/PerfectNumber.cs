804a34209fd481b9de9a8cc5469148d4d0b2b23f
patelshivani3 <shivpatel3001@gmail.com>
2022-12-31 22:21:43 +0530

    Adding Perfect Number

---
+++ LogicalProgramsDay6/PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsDay6
+{
+    internal class PerfectNumber
+    {
+        public static void Number()
+        {
+            int sum = 0 , i = 1;
+            Console.WriteLine("Enter the Number to check perfect Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (i < num)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                    Console.Write(" " + i);
+                }
+                i++;
+            }
+            Console.WriteLine("\nSum of factors :" + sum);
+            if (sum == num)
+            {
+                Console.WriteLine("{0} is a Perfect Number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Perfect Number", num);
+            }
+        }
+    }
+}
--- LogicalProgramsDay6/Program.cs
+++ LogicalProgramsDay6/Program.cs
@@ -11,13 +11,16 @@ namespace LogicalProgramsDay6
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Logical Programs.");
-            Console.WriteLine("\n1.Fibonacci Series");
+            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
-                // Fibonacci Series
                 case 1:
                     FibonacciSeries.Series();
+                    break;
+
+                case 2:
+                    PerfectNumber.Number();
                     break;
 
                 default:

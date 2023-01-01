09d26d34be306b048a4476c3179ee7ccbbca2380
patelshivani3 <shivpatel3001@gmail.com>
2023-01-01 21:12:57 +0530

    Adding Prime Number

---
+++ LogicalProgramsDay6/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsDay6
+{
+    internal class PrimeNumber
+    {
+        public static void Number()
+        {
+
+            int num = 1;
+            Console.WriteLine("Enter a number to check Prime : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    num = 0;
+                }
+            }
+            if (num == 1)
+            {
+                Console.WriteLine("It is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Prime Number");
+            }
+        }
+    }
+}
--- LogicalProgramsDay6/Program.cs
+++ LogicalProgramsDay6/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProgramsDay6
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Logical Programs.");
-            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number");
+            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -23,6 +23,9 @@ namespace LogicalProgramsDay6
                     PerfectNumber.Number();
                     break;
 
+                case 3:
+                    PrimeNumber.Number();
+                    break;  
                 default:
                     Console.WriteLine("Choose from above options only.");
                     break;

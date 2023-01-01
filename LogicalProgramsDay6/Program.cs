237e73313a9a168a8416912f24af4844926b1e06
patelshivani3 <shivpatel3001@gmail.com>
2023-01-01 21:25:50 +0530

    Adding Reverse Number

--- LogicalProgramsDay6/Program.cs
+++ LogicalProgramsDay6/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProgramsDay6
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Logical Programs.");
-            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number");
+            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -26,6 +26,11 @@ namespace LogicalProgramsDay6
                 case 3:
                     PrimeNumber.Number();
                     break;  
+
+                case 4:
+                    ReverseNumber.Number();
+                    break;
+
                 default:
                     Console.WriteLine("Choose from above options only.");
                     break;
---
+++ LogicalProgramsDay6/ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsDay6
+{
+    internal class ReverseNumber
+    {
+        public static void Number()
+        {
+            int rem = 0, rev = 0;
+            Console.WriteLine("Enter a Number to make it reverse : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while (number != 0)
+            {
+                rem = number % 10;
+                rev = rev * 10 + rem;
+                number = number / 10;
+            }
+            Console.WriteLine("Reverse of number is : {0}.", rev);
+        }
+    }
+}

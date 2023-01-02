2c80d1f705241b0653ef0f616bb36eb5c6399585
patelshivani3 <shivpatel3001@gmail.com>
2023-01-02 23:01:23 +0530

    Adding Monthly Payment Program

---
+++ LogicalProgramsDay6/MonthlyPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsDay6
+{
+    internal class MonthlyPayment
+    {
+        public static void GetPayment()
+        {
+            Console.WriteLine("Enter principal amount :");
+            double P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter year :");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter rate of interest:");
+            double R = Convert.ToDouble(Console.ReadLine());
+
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double payment = (P * r) / (1 - Math.Pow((1 + r), -n));
+            Console.WriteLine("Monthly payment is : " + payment);
+
+        }
+    }
+}
--- LogicalProgramsDay6/Program.cs
+++ LogicalProgramsDay6/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProgramsDay6
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Logical Programs.");
-            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n5.Coupon Number\n6.Stimulate Stopwatch\n7.Notes Vending Machine\n8.Days Of Week\n9.Temperature Conversion");
+            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n5.Coupon Number\n6.Stimulate Stopwatch\n7.Notes Vending Machine\n8.Days Of Week\n9.Temperature Conversion\n10.Monthly Payment");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -47,8 +47,13 @@ namespace LogicalProgramsDay6
                 case 8:
                     DayOfWeek.Days();
                     break;
+
                 case 9:
                     TemperatureConversion.TempConvert();
+                    break;
+
+                case 10:
+                    MonthlyPayment.GetPayment();
                     break;
 
                 default:

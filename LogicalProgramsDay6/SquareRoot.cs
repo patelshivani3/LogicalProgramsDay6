4b7b57dfb7eae0126ca7a2ccd7049e68911bc489
patelshivani3 <shivpatel3001@gmail.com>
2023-01-02 23:07:46 +0530

    Adding Square Root Program

---
+++ LogicalProgramsDay6/SquareRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsDay6
+{
+    internal class SquareRoot
+    {
+        public static void GetSquareRoot()
+        {
+            double epsilon = 1e-15, t, sqRt;
+            Console.WriteLine("Enter a number :");
+            int c = Convert.ToInt32(Console.ReadLine());
+            t = c;
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2;
+            }
+            sqRt = Math.Round(t, 2);
+            Console.WriteLine(sqRt);
+
+        }
+    }
+}

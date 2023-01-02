0ffe962df0697017c9588293320e954235879940
patelshivani3 <shivpatel3001@gmail.com>
2023-01-02 17:36:02 +0530

    Adding Notes Vending Machine Program

--- LogicalProgramsDay6/Program.cs
+++ LogicalProgramsDay6/Program.cs
@@ -11,12 +11,13 @@ namespace LogicalProgramsDay6
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Logical Programs.");
-            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n5.Coupon Number\n6.Stimulate Stopwatch");
+            Console.WriteLine("\n1.Fibonacci Series\n2.Perfect Number\n3.Prime Number\n4.Reverse Number\n5.Coupon Number\n6.Stimulate Stopwatch\n7.Notes Vending Machine");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.Series();
                     break;
 
                 case 2:
@@ -39,6 +40,11 @@ namespace LogicalProgramsDay6
                     StopWatch.GetElapsedTime();
                     break;
 
+                case 7:
+                    VendingMachine.GetNotes();
+                    break;
+
+                
 
                 default:
                     Console.WriteLine("Choose from above options only.");
---
+++ LogicalProgramsDay6/VendingMachine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsDay6
+{
+    internal class VendingMachine
+    {
+        
+        //public static int[] notes = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+        //public static int quotient = 0;
+        //public static int Count = 0;
+        public static void GetNotes()
+        {
+            int[] notes = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int quotient = 0;
+            int Count = 0;
+            Console.WriteLine("Enter the amount to be returned by the Vending Machine :");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    quotient = amount / notes[i];
+                    amount = amount - (quotient * notes[i]); 
+                    Console.WriteLine("{0}*{1}:{2}", notes[i], quotient, notes[i] * quotient); 
+                    Count = Count + quotient;
+                }
+            }
+            Console.WriteLine("Minimum number of notes : " + Count);
+        }
+    }
+}
